81a3514c972e9ce0c7b747ec26af4a9fb3391596
Kyle Kelley <rgbkrk@gmail.com>
2014-10-13 21:32:06 -0700

    Allow for example.org/user-blah

--- orchestrate.py
+++ orchestrate.py
@@ -219,8 +219,8 @@ def main():
 
     handlers = [
         (r"/", LoadingHandler),
-        (r"/spawn/?(/user-\w+/.+)?", SpawnHandler),
-        (r"/(user-\w+)/.*", LoadingHandler),
+        (r"/spawn/?(/user-\w+(?:/.*)?)?", SpawnHandler),
+        (r"/(user-\w+)(?:/.*)?", LoadingHandler),
     ]
 
     proxy_token = os.environ['CONFIGPROXY_AUTH_TOKEN']
