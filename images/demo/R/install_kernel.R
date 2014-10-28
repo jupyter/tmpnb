af9266aaebf2d3ad4a5e6363a3902d30fc1af932
Kyle Kelley <rgbkrk@gmail.com>
2014-10-28 21:59:07 +0000

    Pull from rzmq master

--- images/demo/R/install_kernel.R
+++ images/demo/R/install_kernel.R
@@ -3,7 +3,7 @@ install.packages('devtools')
 
 library(devtools)
 
-install_github('armstrtw/rzmq#8')
+install_github('armstrtw/rzmq')
 install_github("takluyver/IRdisplay")
 install_github("takluyver/IRkernel")
 
