8a3fe128c5e436aba5ad212435e9ae00efe89694
Safia Abdalla <safia@safia.rocks>
2015-08-30 20:21:14 -0400

    Added docker and container information to stats API

--- orchestrate.py
+++ orchestrate.py
@@ -69,19 +69,26 @@ class LoadingHandler(BaseHandler):
         self.render("loading.html", path=path)
 
 
-class StatsAPIHandler(BaseHandler):
+class APIStatsHandler(BaseHandler):
     def get(self):
         '''Returns some statistics/metadata about the tmpnb server'''
         response = {
                 'available': len(self.pool.available),
                 'capacity': self.pool.capacity,
-                'version': '0.0.1-dev',
+                'version': '0.1.0',
+                'container_image': self.pool.container_config.image,
+                'docker_version': self.spawner.docker_client._docker_client.version(),
+                'docker_containers': self.spawner.docker_client._docker_client.containers()
         }
         self.write(response)
 
     @property
     def pool(self):
         return self.settings['pool']
+
+    @property
+    def spawner(self):
+        return self.settings['spawner']
 
 
 class SpawnHandler(BaseHandler):
@@ -232,7 +239,7 @@ def main():
         (r"/spawn/?(/user/\w+(?:/.*)?)?", SpawnHandler),
         (r"/api/spawn/", APISpawnHandler),
         (r"/(user/\w+)(?:/.*)?", LoadingHandler),
-        (r"/stats/api", StatsAPIHandler),
+        (r"/api/stats", APIStatsHandler),
     ]
 
     proxy_token = os.environ['CONFIGPROXY_AUTH_TOKEN']
